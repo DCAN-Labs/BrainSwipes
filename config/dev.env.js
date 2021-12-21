c48312e0eafa9bf27853ced9af02e7d4bddb6b6b
Barry J. Tikalsky <tikal004@umn.edu>
2021-12-21 15:27:39 -0600

    removed env variable references

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_AWS_ID: process.env.VUE_APP_AWS_ID,
-  VUE_APP_AWS_KEY: process.env.VUE_APP_AWS_KEY
+  NODE_ENV: '"development"'
 })
