050805ad7d03e3cb2d3937eac0adb7bf5606f2b0
Barry J. Tikalsky <tikal004@umn.edu>
2022-01-21 17:07:32 -0600

    removed functions used for other manifest types

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -5,55 +5,15 @@
     <b-container>
 
       <p class="lead" v-if="status=='complete'">You have {{sampleCounts.length}} items currently</p>
-      <p v-if="manifestType === 'json'">
+      <p>
         <b>Data Source:</b>
         <a :href="config.manifestUrl" target="_blank">Manifest JSON</a>
       </p>
-
-      <p v-else-if="manifestType === 'pubmed'">
-        <b>Pubmed Query:</b>
-        {{config.manifestQuery}}
-      </p>
-
-      <p v-else-if="manifestType === 'github'">
-        <b>Github User:</b>
-        {{config.manifestGitHub.user}}
-        <br>
-        <b>Github Repo:</b>
-        {{config.manifestGitHub.repo}}
-        <br>
-        <b>Github Path:</b>
-        {{config.manifestGitHub.path}}
-        <br>
-      </p>
-
-      <p v-else-if="manifestType === 'S3'">
-        <b>S3 bucket:</b>
-        {{config.manifestS3.bucket}}
-        <br>
-        <b>prefix:</b>
-        {{config.manifestS3.prefix}}
-        <br>
-        <b>delimiter:</b>
-        {{config.manifestS3.delimiter}}
-        <br>
-      </p>
-
-
       <b-button variant="warning" @click="previewManifest">
         <span> Preview </span>
       </b-button>
       <div v-if="manifestEntries.length" class="mt-3 pt-3">
-        <small v-if="manifestType === 'json'">
-          Here are a few items in your manifest file. There are {{manifestEntries.length}} items in total
-        </small>
-        <small v-else-if="manifestType === 'pubmed'">
-          Here are a few pubmed IDs from your search. There are {{pubmedQueryStore.count}} items in total.
-        </small>
-        <small v-if="manifestType === 'github'">
-          Here are a few items in your manifest file. There are {{manifestEntries.length}} items in total
-        </small>
-        <small v-if="manifestType === 'S3'">
+        <small>
           Here are a few items in your manifest file. There are {{manifestEntries.length}} items in total
         </small>
         <textarea class="mt-3 mb-3 ml-3 mr-3 w-100"
@@ -161,20 +121,6 @@ export default {
   mounted() {
     this.addFirebaseListener();
   },
-  computed: {
-    /**
-     * The manifest type can be either 'json' (default) or 'pubmed'
-     */
-    manifestType() {
-      let manifestType = null;
-      if (!this.config.manifestType) {
-        manifestType = 'json';
-      } else {
-        manifestType = this.config.manifestType;
-      }
-      return manifestType;
-    },
-  },
   methods: {
     /**
      * This method keeps track of sampleCounts, but only loads it once.
@@ -190,134 +136,14 @@ export default {
       });
     },
     /**
-    * XML parser for pubmed query returns.
-    */
-    xmlParser(input) {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(input, 'text/xml');
-      return xmlDoc;
-    },
-    /**
-    * if the manifest type is pubmed, then we need to query pubmed's API
-    * to store our search on the server, and preview the first 100 pmids.
-    */
-    getPubmedQueryPreview() {
-      const baseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi';
-      const url = `${baseUrl}?db=pubmed&term=${encodeURI(this.config.manifestQuery)}&usehistory=y&retmax=100`;
-      return axios.get(url).then((resp) => {
-        const xml = this.xmlParser(resp.data);
-        const webEnv = xml.getElementsByTagName('WebEnv')[0];
-        const count = xml.getElementsByTagName('Count')[0];
-        const ids = xml.getElementsByTagName('IdList')[0].children;
-        this.pubmedQueryStore.webEnv = webEnv.innerHTML;
-        this.pubmedQueryStore.count = count.innerHTML;
-        this.manifestEntries = _.map(ids, i => i.innerHTML);
-      });
-    },
-    /**
-    * after the pubmed query preview, we need to get the full list of IDs
-    * and save them to the database.
-    */
-    getPubmedQueryFull() {
-      const baseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi';
-      if (!this.pubmedQueryStore.webEnv) {
-        this.getPubmedQueryPreview().then(() => {
-          this.getPubmedQueryFull();
-        });
-      } else {
-        const url = `${baseUrl}?db=pubmed&term=${encodeURI(this.config.manifestQuery)}&usehistory=y&retmax=${this.pubmedQueryStore.count}&WebEnv=${this.pubmedQueryStore.webEnv}`;
-        axios.get(url).then((resp) => {
-          const xml = this.xmlParser(resp.data);
-          const ids = xml.getElementsByTagName('IdList')[0].children;
-          this.manifestEntries = _.map(ids, i => i.innerHTML);
-          // console.log(this.manifestEntries);
-          this.syncEntries();
-        });
-      }
-    },
-    /**
-    * get a list of files from a github pointer
-    * TODO: make sure the filenames are firebase compatible, and if they aren't convert them.
-    */
-    getGithubManifest() {
-      let url = `https://api.github.com/repos/${this.config.manifestGitHub.user}/`;
-      url += `${this.config.manifestGitHub.repo}/contents/${this.config.manifestGitHub.path}`;
-      return axios.get(url).then((resp) => {
-        this.manifestEntries = _.map(resp.data, v => v.name.split('.')[0]);
-      });
-    },
-    /**
-    *
-    */
-    parseS3(data) {
-      const xml = this.xmlParser(data);
-      const keys = xml.getElementsByTagName('Key');
-      const continuation = xml.getElementsByTagName('NextContinuationToken');
-      const isTruncated = xml.getElementsByTagName('IsTruncated')[0].innerHTML;
-      if (isTruncated === 'true') {
-        this.continuation = encodeURIComponent(continuation[0].innerHTML);
-      } else {
-        this.continuation = null;
-      }
-      const allKeys = _.map(keys, k => k.innerHTML);
-      const keysFiltered = _.filter(allKeys, k => k.replace(`${this.config.manifestS3.prefix}/`, ''));
-      const keysFixed = _.map(keysFiltered, k => k.replace(`${this.config.manifestS3.prefix}/`, '').split('.')[0]);
-      return keysFixed;
-    },
-    /**
-    * if there is a continuation token..
-    */
-    S3Continuation(token) {
-      let url = `https://s3-us-west-2.amazonaws.com/${this.config.manifestS3.bucket}/?list-type=2&`;
-      url += `prefix=${this.config.manifestS3.prefix}/&max-keys=${this.config.manifestS3.max_keys}`;
-      url += `&delimiter=${this.config.manifestS3.delimiter}`;
-      url += `&continuation-token=${token}`;
-      if (!token) {
-        return 0;
-      }
-      return axios.get(url).then((resp) => {
-        const keysFixed2 = this.parseS3(resp.data);
-        this.manifestEntries = _.uniq(this.manifestEntries.concat(keysFixed2));
-        if (this.continuation) {
-          this.S3Continuation(this.continuation);
-        }
-      });
-    },
-    /**
-    * get a list of files that are in a bucket of an S3
-    * with a prefix and a delimiter (usually, a .)
-    * TODO: make the keys firebase safe!!
-    */
-    getS3Manifest() {
-      let url = `https://s3-us-west-2.amazonaws.com/${this.config.manifestS3.bucket}/?list-type=2&`;
-      url += `prefix=${this.config.manifestS3.prefix}/&max-keys=${this.config.manifestS3.max_keys}`;
-      url += `&delimiter=${this.config.manifestS3.delimiter}`;
-      // console.log(url);
-      return axios.get(url).then((resp) => {
-        const keysFixed = this.parseS3(resp.data);
-        this.manifestEntries = _.uniq(this.manifestEntries.concat(keysFixed));
-        if (this.continuation) {
-          this.S3Continuation(this.continuation);
-        }
-      });
-    },
-    /**
      * A method that fetches the manifest so the user can see what's in it.
      * TODO: add a .catch event and display an error if something goes wrong
      * with this request.
      */
     previewManifest() {
-      if (this.manifestType === 'json') {
-        axios.get(this.config.manifestUrl).then((resp) => {
-          this.manifestEntries = resp.data;
-        });
-      } else if (this.manifestType === 'pubmed') {
-        this.getPubmedQueryPreview();
-      } else if (this.manifestType === 'github') {
-        this.getGithubManifest();
-      } else if (this.manifestType === 'S3') {
-        this.getS3Manifest();
-      }
+      axios.get(this.config.manifestUrl).then((resp) => {
+        this.manifestEntries = resp.data;
+      });
     },
     /**
      * this method runs in a worker, to check each item in /sampleCounts and each
@@ -326,32 +152,13 @@ export default {
      */
     refreshSamples() {
       this.status = 'refreshing';
-      if (this.manifestType === 'json') {
-        // grab all the data from the json file defined in the config
-        axios.get(this.config.manifestUrl).then((resp) => {
-          // resp.data has a list of firebase-friendly strings
-          const manifestEntries = resp.data;
-          this.manifestEntries = manifestEntries;
-          this.syncEntries();
-        });
-      } else if (this.manifestType === 'pubmed') {
-        this.getPubmedQueryFull();
-      } else if (this.manifestType === 'github') {
-        this.getGithubManifest().then(() => {
-          this.syncEntries();
-        });
-      } else if (this.manifestType === 'S3') {
-        // this.getS3Manifest().then(() => {
-        if (this.manifestEntries.length) {
-          this.syncEntries();
-        } else {
-          // eslint-disable-next-line
-          alert('please press the preview button first,' +
-          ' and wait to get the final count');
-          this.status = 'complete';
-        }
-        // });
-      }
+      // grab all the data from the json file defined in the config
+      axios.get(this.config.manifestUrl).then((resp) => {
+        // resp.data has a list of firebase-friendly strings
+        const manifestEntries = resp.data;
+        this.manifestEntries = manifestEntries;
+        this.syncEntries();
+      });
     },
     /**
     * sync manifest entries and firebase entries
