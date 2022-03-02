07b05fac2fba3bf166ac46f2b3acc28debd43487
Barry J. Tikalsky <tikal004@umn.edu>
2022-03-02 11:56:57 -0600

    rough draft of viz

--- src/components/Visualizations/InterraterConcordance.vue
+++ src/components/Visualizations/InterraterConcordance.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="InterraterConcordance">
-    <apexchart type="heatmap" height="200" :options="chartOptions" :series="series"></apexchart>
+  <div v-if="loading">
+    LOADING
+  </div>
+  <div v-else id="InterraterConcordance">
+    <apexchart ref="heatmap" type="heatmap" height="650" :options="chartOptions" :series="chartData"></apexchart>
   </div>
 
 </template>
@@ -12,6 +15,7 @@
 <script>
   import Vue from 'vue';
   import VueApexCharts from 'vue-apexcharts';
+  import colorGradient from 'javascript-color-gradient';
 
   Vue.use(VueApexCharts);
 
@@ -31,40 +35,50 @@
             toolbar: {
               show: false,
             },
+            animations: {
+              enabled: false,
+            },
           },
           dataLabels: {
             enabled: false,
           },
-          colors: ['#008FFB'],
+          colors: ['#000000'],
           title: {
-            text: 'Inter-Rater Concordance',
+            text: 'Inter-Rater Concordance (percent agreement)',
+          },
+          tooltip: {
+            y: {
+              formatter(value) {
+                if (value === 200) {
+                  return 'No Data';
+                }
+                return `${value}%`;
+              },
+            },
+          },
+          plotOptions: {
+            heatmap: {
+              colorScale: {
+                ranges: [{
+                  from: 199,
+                  to: 201,
+                  color: '#FFFFFF',
+                  name: ' ',
+                }],
+              },
+            },
+          },
+          xaxis: {
+            type: 'category',
+            categories: [],
           },
         },
-        series: [{
-          name: 'Metric1',
-          data: [90, 70, 20, 90, 20, 90, 20, 20, 30, 40, 60, 10, 90, 0, 100, 14, 46, 23],
-        },
-        {
-          name: 'Metric2',
-          data: [70, 20, 90, 70, 20, 90, 20, 20, 40, 60, 10, 90, 0, 100, 14, 46, 23, 90],
-        },
-        {
-          name: 'Metric3',
-          data: [90, 70, 20, 90, 70, 20, 20, 20, 30, 40, 60, 10, 90, 0, 100, 14, 46, 23],
-        },
-        {
-          name: 'Metric4',
-          data: [90, 70, 20, 90, 70, 20, 90, 20, 20, 30, 40, 60, 10, 90, 0, 14, 46, 23],
-        },
-        {
-          name: 'Metric5',
-          data: [90, 70, 20, 90, 70, 20, 90, 20, 20, 40, 60, 10, 90, 0, 100, 14, 46, 23],
-        },
-        {
-          name: 'Metric6',
-          data: [90, 70, 20, 70, 20, 90, 20, 20, 30, 40, 60, 10, 90, 0, 100, 14, 46, 23],
-        },
-        ],
+        /**
+         * data from the db
+         */
+        chartData: [],
+        loading: true,
+        categories: [],
       };
     },
     props: {
@@ -81,6 +95,59 @@
       db: {
         type: Object,
         required: true,
+      },
+    },
+    created() {
+      this.setColors();
+      this.getData(500);
+    },
+    methods: {
+      async getData(min) {
+        const ref = this.db.ref('datasets/BCP/visualizations/interRaterConcordance');
+        const snap = await ref.once('value');
+        Object.keys(snap.val()).forEach((user1) => {
+          if (snap.val()[user1][user1][0] >= min) {
+            this.categories.push(user1);
+            const series = [];
+            Object.keys(snap.val()[user1]).forEach((user2) => {
+              if (snap.val()[user2][user2][0] >= min) {
+                const scores = snap.val()[user1][user2];
+                let ratio = scores[0] / scores[1];
+                if (!ratio) {
+                  ratio = 2;
+                }
+                series.push(100 * ratio.toFixed(2));
+              }
+            });
+            this.chartData.push({
+              name: [user1],
+              data: series,
+            });
+          }
+        });
+        this.chartOptions.xaxis = { type: 'category', categories: this.categories };
+        this.loading = false;
+      },
+      setColors() {
+        const gradientArray = colorGradient
+          .setGradient('#440155', '#238a8d', '#fde725')
+          .setMidpoint(20)
+          .getArray();
+        const ranges = [];
+        for (let i = 0; i < gradientArray.length; i += 1) {
+          ranges.push({
+            from: (100 / gradientArray.length) * i,
+            to: (100 / gradientArray.length) * (i + 1),
+            color: gradientArray[i],
+          });
+        }
+        ranges.push({
+          from: 199,
+          to: 201,
+          color: '#FFFFFF',
+          name: 'No Data',
+        });
+        this.chartOptions.plotOptions.heatmap.colorScale.ranges = ranges;
       },
     },
   };
