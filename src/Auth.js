985d265c0dd1a32a31b76245c5168e89a25bd622
Barry Tikalsky <tikal004@umn.edu>
2022-05-04 17:14:54 -0500

    receives globus token, identities

--- src/Auth.js
+++ src/Auth.js
@@ -2,11 +2,11 @@ import PKCE from 'js-pkce';
 
 // Reference: https://github.com/bpedroza/js-pkce
 const PkceAuth = new PKCE({
-  client_id: 'd032f59c-b5a3-4c27-9c38-adbd94a63e9a',  // Update this using your native client ID
+  client_id: 'ec1482f4-69f6-435b-97b2-d243f39babb3',  // Update this using your native client ID
   redirect_uri: 'http://localhost:8080/restricted',  // Update this if you are deploying this anywhere else (Globus Auth will redirect back here once you have logged in)
   authorization_endpoint: 'https://auth.globus.org/v2/oauth2/authorize',  // No changes needed
   token_endpoint: 'https://auth.globus.org/v2/oauth2/token',  // No changes needed
-  requested_scopes: 'openid profile email'  // Update with any scopes you would need, e.g. transfer
-})
+  requested_scopes: 'openid profile email',  // Update with any scopes you would need, e.g. transfer
+});
 
 export default PkceAuth;
--- src/components/Restricted.vue
+++ src/components/Restricted.vue
@@ -1,10 +1,12 @@
 <template>
-    <div v-if="!authenticated">
-        <b-button class="btn btn-primary" @click="loginWithGlobus">Login with Globus</b-button>
-    </div>
-    <div v-else>
-        <p>{{authToken}}</p>
-    </div>
+  <div v-if="!authenticated">
+    <b-button class="btn btn-primary" @click="loginWithGlobus">Login with Globus</b-button>
+  </div>
+  <div v-else>
+    <b-button class="btn btn-primary" @click="logoutOfGlobus">Logout of Globus</b-button>
+    <p>{{authToken}}</p>
+    <b-button class="btn btn-warn" @click="getUserInfo">Get User Info</b-button>
+  </div>
 </template>
 
 <script>
@@ -12,70 +14,83 @@
 import PkceAuth from '../Auth';
 
 export default {
-    name: 'restricted',
-    data() {
-        return {
-            authToken: '',
-            rawIdToken: '',
-            idToken: '',
-            authenticated: false,
-        }
+  name: 'restricted',
+  data() {
+    return {
+      authToken: '',
+      rawIdToken: '',
+      idToken: '',
+      authenticated: false,
+      globusUserInfo: {},
+    };
+  },
+  methods: {
+    loginWithGlobus() {
+      const authUrl = PkceAuth.authorizeUrl();
+      console.log(`Sending the user to ${authUrl}`);
+      window.location.replace(authUrl);
     },
-    methods: {
-        loginWithGlobus() {
-            const authUrl = PkceAuth.authorizeUrl();
-            console.log(`Sending the user to ${authUrl}`);
-            window.location.replace(PkceAuth.authorizeUrl());
-        },
-        checkForGlobusAuthCode() {
-            const params = new URLSearchParams(window.location.search);
-            const authCode = params.get("code");
-            if (authCode) {
-                console.log("Getting a token...");
-                const url = window.location.href;
-                console.log(url);
-                console.log(PkceAuth.config);
-                PkceAuth.exchangeForAccessToken(url).then((resp) => {
-                    console.log(resp);
-                    // If you get back multiple tokens you'll need to make changes here.
-                    const accessToken = resp.access_token;
-                    const idToken = resp.id_token;
+    checkForGlobusAuthCode() {
+      const params = new URLSearchParams(window.location.search);
+      const authCode = params.get('code');
+      if (authCode) {
+        console.log('Getting a token...');
+        const url = window.location.href;
+        PkceAuth.exchangeForAccessToken(url).then((resp) => {
+          console.log(resp);
+          // If you get back multiple tokens you'll need to make changes here.
+          const accessToken = resp.access_token;
+          const idToken = resp.id_token;
 
-                    // Set it in local storage - the are a number of alternatives for
-                    // saving this that are arguably more secure but this is the simplest
-                    // for demonstration purposes.
-                    window.localStorage.setItem("authToken", accessToken);
-                    window.localStorage.setItem("idToken", idToken);
+          // Set it in local storage - the are a number of alternatives for
+          // saving this that are arguably more secure but this is the simplest
+          // for demonstration purposes.
+          window.localStorage.setItem('authToken', accessToken);
+          window.localStorage.setItem('idToken', idToken);
 
-                    // This isn't strictly necessary but it ensures no code reuse.
-                    sessionStorage.removeItem('pkce_code_verifier');
-                    sessionStorage.removeItem('pkce_state');
-                    console.log('Cleared the PKCE state!');
+          // This isn't strictly necessary but it ensures no code reuse.
+          sessionStorage.removeItem('pkce_code_verifier');
+          sessionStorage.removeItem('pkce_state');
+          console.log('Cleared the PKCE state!');
 
-                    // Redirect back to the root URL (simple but brittle way to clear the query params)
-                    this.$router.push({ name: 'Restricted'});
-                });  
-            }
-        },
-        logoutOfGlobus() {
-            // Should revoke here
-            window.localStorage.removeItem("authToken");
-            window.localStorage.removeItem("idToken");
-            this.$router.push({ name: 'Home'});
-        },
-        allowAccess() {
-            this.authToken = localStorage.getItem("authToken");
-            this.rawIdToken = localStorage.getItem("idToken");
-            if (this.authToken) {
-                this.authenticated = true;
-            }
-        }
+          // Redirect back to the root URL (simple but brittle way to clear the query params)
+          this.$router.push({ name: 'Restricted' });
+        });
+      }
     },
-    created() {
-        this.checkForGlobusAuthCode();
-        this.allowAccess();
+    logoutOfGlobus() {
+      // Should revoke here
+      window.localStorage.removeItem('authToken');
+      window.localStorage.removeItem('idToken');
+      this.$router.push({ name: 'Home' });
     },
-}
+    allowAccess() {
+      this.authToken = localStorage.getItem('authToken');
+      this.rawIdToken = localStorage.getItem('idToken');
+      if (this.authToken) {
+        this.authenticated = true;
+      }
+    },
+    getUserInfo() {
+      if (this.authToken) {
+        console.log('Getting user information with token !');
+        fetch('https://auth.globus.org/p/whoami?include=identity_provider', {
+          headers: new Headers({
+            Authorization: `Bearer ${this.authToken}`,
+          }),
+        }).then(response => response.json())
+        .then((responseData) => {
+          this.globusUserInfo = responseData.identities;
+          console.log(this.globusUserInfo);
+        });
+      }
+    },
+  },
+  created() {
+    this.checkForGlobusAuthCode();
+    this.allowAccess();
+  },
+};
 </script>
 
 <style>
