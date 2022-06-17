968c52b785734ee0d45d42be8bf111b2d27ebfb0
Barry Tikalsky <tikal004@umn.edu>
2022-06-17 14:59:03 -0500

    added definition hover for text

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -11,7 +11,21 @@
       <b-progress :value="scrollPosition" :max="1" show-progress class="ml-3 mr-3"></b-progress>
     </div>
 
-    <b-button id="help" @click="openGlossary">?</b-button>
+    <b-button type="button" class="btn btn-outline-primary btn-light" id="open-toc" @click="openTOC">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg></b-button>
+    <div id="table-of-contents" v-show="showTOC">
+      <ul>
+        <li v-scroll-to="'#mri-intro'">MRI Introduction</li>
+        <li v-scroll-to="'#qa-image-types'">QA Image Types</li>
+        <li v-scroll-to="'#structural-qa'">How to perform structural QA</li>
+        <li v-scroll-to="'#functional-qa'">How to perform functional QA</li>
+        <li v-scroll-to="'#using-swipes'">Using the BrainSwipes interface</li>
+      </ul>
+    </div>
+
+    <b-button id="help" class="btn btn-outline-primary btn-light" @click="openGlossary">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/></svg>    
+    </b-button>
 
     <!-- Glossary -->
     <b-modal id="glossary" :title="'Glossary of Terms'"
@@ -150,7 +164,28 @@
     top: 5vh;
     right: 5vw;
     z-index: 5;
-    border-radius: 50%;
+  }
+
+  #open-toc {
+    position: fixed;
+    top: 5vh;
+    left: 0;
+    z-index: 5;
+  }
+
+  #table-of-contents {
+    position: fixed;
+    top: 10vh;
+    left: 0;
+    z-index: 6;
+    outline: 1px solid black;
+    padding: 0.5em 1em 1em 1em;
+    background-color: white;
+  }
+
+  #table-of-contents li{
+    margin-top: 0.5em;
+    cursor: pointer;
   }
 
   .tutorial-steps h1{
@@ -165,6 +200,11 @@
 
   .arrow{
     display: block;
+  }
+
+  dfn{
+    font-weight: bold;
+    cursor: help;
   }
 
   @media (min-width: 65em) {
@@ -224,6 +264,10 @@
          * Glossary of terms
          */
         glossary,
+        /**
+         * variable that opens and closes the table of contents
+         */
+        showTOC: false,
       };
     },
     props: {
@@ -347,6 +391,34 @@
       openGlossary() {
         this.$refs.glossary.show();
       },
+      openTOC() {
+        this.showTOC = !this.showTOC;
+      },
+      addDefinitions() {
+        let terms = [];
+        Object.keys(this.glossary).forEach((category) => {
+          terms = terms.concat(Object.keys(this.glossary[category]));
+        });
+        console.log(terms);
+        Object.keys(this.steps).forEach((category) => {
+          console.log(category);
+          Object.keys(category).forEach((step) => {
+            if (Object.hasOwnProperty.call(this.steps[category], step)) {
+              // console.log(this.steps[category][step].text);
+              terms.forEach((term) => {
+                const match = this.steps[category][step].text.toString().toUpperCase().match(term.toString().toUpperCase());
+                if (match) {
+                  console.log(match);
+                  const regex = new RegExp(term,"gi");
+                  const result = this.steps[category][step].text.toString().replace(regex, `<dfn title="test">${term}</dfn>`);
+                  console.log(result);
+                  this.steps[category][step].text = result;
+                }
+              });
+            }
+          });
+        });
+      },
     },
     /**
      * Add a scroll listener when the component is created.
@@ -359,6 +431,9 @@
      */
     destroyed() {
       window.removeEventListener('scroll', this.handleScroll);
+    },
+    mounted() {
+      this.addDefinitions();
     },
   };
 </script>
