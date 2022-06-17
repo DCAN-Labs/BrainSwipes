7d337fdfabd56323f55456fc4d865cc2fa6bbecc
Barry Tikalsky <tikal004@umn.edu>
2022-06-17 16:35:00 -0500

    some work applying dfn tags

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -11,6 +11,7 @@
       <b-progress :value="scrollPosition" :max="1" show-progress class="ml-3 mr-3"></b-progress>
     </div>
 
+    <!-- Table of Contents -->
     <b-button type="button" class="btn btn-outline-primary btn-light" id="open-toc" @click="openTOC">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg></b-button>
     <div id="table-of-contents" v-show="showTOC">
@@ -395,27 +396,21 @@
         this.showTOC = !this.showTOC;
       },
       addDefinitions() {
-        let terms = [];
         Object.keys(this.glossary).forEach((category) => {
-          terms = terms.concat(Object.keys(this.glossary[category]));
-        });
-        console.log(terms);
-        Object.keys(this.steps).forEach((category) => {
-          console.log(category);
-          Object.keys(category).forEach((step) => {
-            if (Object.hasOwnProperty.call(this.steps[category], step)) {
-              // console.log(this.steps[category][step].text);
-              terms.forEach((term) => {
-                const match = this.steps[category][step].text.toString().toUpperCase().match(term.toString().toUpperCase());
-                if (match) {
-                  console.log(match);
-                  const regex = new RegExp(term,"gi");
-                  const result = this.steps[category][step].text.toString().replace(regex, `<dfn title="test">${term}</dfn>`);
-                  console.log(result);
-                  this.steps[category][step].text = result;
-                }
-              });
-            }
+          const terms = Object.keys(this.glossary[category]);
+          Object.keys(this.steps).forEach((section) => {
+            Object.keys(section).forEach((step) => {
+              if (Object.hasOwnProperty.call(this.steps[section], step)) {
+                terms.forEach((term) => {
+                  const match = this.steps[section][step].text.toString().toUpperCase().match(term.toString().toUpperCase());
+                  if (match) {
+                    const regex = new RegExp(term, 'gi');
+                    const result = this.steps[section][step].text.toString().replace(regex, `<dfn title="${this.glossary[category][term]}">${term}</dfn>`);
+                    this.steps[section][step].text = result;
+                  }
+                });
+              }
+            });
           });
         });
       },
