fc5f39b9fe3ae6807b33454058e05f3e685a0b3a
Barry Tikalsky <tikal004@umn.edu>
2024-02-29 12:12:52 -0600

    enforce argument patterns, more prints

--- tools/images/ingest_brainswipes_data.py
+++ tools/images/ingest_brainswipes_data.py
@@ -7,47 +7,91 @@ import shutil
 import numpy as np
 from PIL import Image
 
+def s3_path(arg_value):
+    '''
+    Argparse type for enforcing s3 path patterns on the command line
+    '''
+    pattern = re.compile(r"^s3:\/\/.+\/$")
+    if not pattern.match(arg_value):
+        raise argparse.ArgumentTypeError(f"{arg_value} is not a valid S3 path. 's3://BUCKET/' or 's3://BUCKET/OPTIONAL/PATH/")
+    return arg_value
+
+def trailing_slash(arg_value):
+    '''
+    Argparse type for enforcing path fragment patterns on the command line
+    '''
+    pattern = re.compile(r"^[^\/].*\/$")
+    if not pattern.match(arg_value):
+        raise argparse.ArgumentTypeError(f"{arg_value} must not start with a '/' character, but must end with a '/' character.")
+    return arg_value
+
 def get_args():
+    '''
+    Use argparse to get command line arguments.
+    '''
     parser = argparse.ArgumentParser(prog="ingest-brainSwipes-data",
                                      description="Gets executive summary files from an s3 bucket for all subjects and sessions, reformats registraions and pushes all .png files to a new s3 bucket.")
-    parser.add_argument("source", help=("The name of the s3 bucket on MSI data is currently in. " "Do not include the prefix 's3://'"))
-    parser.add_argument("subjects_path", help=("The path in the s3 bucket that holds the processed subjects. " "include a trailing '/'" "e.g. processed/abcd-hcp-pipeline/"))
-    parser.add_argument("imgs_path", help=("The path in the processed subject/session that holds the images to be ingested. " "include a trailing '/'" "e.g. files/summary_DCANBOLDProc_v4.0.0/executivesummary/img/"))
-    parser.add_argument("destination", help=("The name of the s3 bucket on MSI data will be placed after transformation. " "Do not include the prefix 's3://'"))
+    parser.add_argument("s3_subjects_path", type=s3_path, help=("The s3 bucket and path to the directory with the subjects list. " "`s3://BUCKET/` or `s3://BUCKET/OPTIONAL/PATH/`"))
+    parser.add_argument("imgs_path", type=trailing_slash, help=("The path in the processed subject/session that holds the images to be ingested. " "include a trailing '/'" "e.g. files/summary_DCANBOLDProc_v4.0.0/executivesummary/img/"))
+    parser.add_argument("destination", type=s3_path, help=("The s3 bucket and path where data will be placed after transformation. " "`s3://BUCKET/` or `s3://BUCKET/OPTIONAL/PATH/`"))
     parser.add_argument("--pipeline", choices=['dcan', 'xcpd'], default="dcan", help=("The pipleine used to generate the executive summary. " "Defaults to dcan"))
     parser.add_argument("--start", type=int, default=1, help=("What number subject to start data ingestion at. Defaults to first subject."))
+    parser.add_argument("--stop", type=int, help=("What number subject to stop data processing at. " "If not included will run all subjects."))
+    parser.add_argument("--no_cleanup", "--no-cleanup", action="store_true", help=("Do not remove intermediary files."))
+
     return parser.parse_args()
 
 def get_subjects(args):
-    subjects = []
-    path_to_subjects = 's3://' + os.path.join(args.source, args.subjects_path)
-    cmd = ['s3cmd', 'ls', path_to_subjects]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    regex = re.compile('(sub-.*)\/')
-    with process.stdout:
-        for line in process.stdout:
-            match = re.search(regex, line)
-            sub = match.group(1)
-            subjects.append(sub)
-    return subjects
+    '''
+    Return a list of all subjects at the user input s3 path
+    '''
+    try:
+        print("Getting subject list...")
+        subjects = []
+        cmd = ['s3cmd', 'ls', args.s3_subjects_path]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        regex = re.compile('(sub-.*)\/')
+        with process.stdout:
+            for line in process.stdout:
+                match = re.search(regex, line)
+                sub = match.group(1)
+                subjects.append(sub)
+        print(f"Found {len(subjects)} subjects")
+        return subjects
+    except Exception as e:
+        print("Error getting subjects:")
+        print(e)
 
 def get_sessions(subject, args):
-    sessions = []
-    path_to_sessions = 's3://' + os.path.join(args.source, args.subjects_path, subject) + '/'
-    cmd = ['s3cmd', 'ls', path_to_sessions]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    regex = re.compile('(ses-.*)\/')
-    with process.stdout:
-        for line in process.stdout:
-            match = re.search(regex, line)
-            ses = match.group(1)
-            sessions.append(ses)
-    return sessions
+    '''
+    Return a list of all sessions under the given subject
+    '''
+    try:
+        print(f"Getting sessions for {subject}")
+        sessions = []
+        path_to_sessions = args.s3_subjects_path + subject + '/'
+        cmd = ['s3cmd', 'ls', path_to_sessions]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        regex = re.compile('(ses-.*)\/')
+        with process.stdout:
+            for line in process.stdout:
+                match = re.search(regex, line)
+                ses = match.group(1)
+                sessions.append(ses)
+        return sessions
+    except Exception as e:
+        print("Error getting sessions:")
+        print(e)
 
 def download_imgs(subject, session, dir, args):
+    '''
+    calls s3cmd as a subprocess to download all files in the user input image path for a specific subject/session
+    '''
     try:
+        print(f"Downloading {subject} {session} from S3")
         os.mkdir(dir)
-        img_path = "s3://" + os.path.join(args.source, args.subjects_path, subject, session, args.imgs_path)
+        img_path = args.s3_subjects_path + subject + '/' + session + '/' + args.imgs_path
+        print(img_path)
         cmd = ['s3cmd', 'get', '--recursive', img_path]
         process = subprocess.check_call(cmd, cwd=f"./{dir}", stdout=subprocess.DEVNULL)
 
@@ -56,14 +100,22 @@ def download_imgs(subject, session, dir, args):
         print(e)
 
 def cleanup(dir):
+    '''
+    deletes all files and subdirectories in a directory then removes the directory
+    '''
     try:
+        print(f"Removing intermdiary files: {dir}")
         shutil.rmtree(dir)
     except Exception as e:
         print("Error cleaning up subject")
         print(e)
 
 def rearrange_image(gif, output_dir, pipeline):
-
+    '''
+    Transforms a image file with a 1x9 array of images into a 3x3 grid.
+    Expects specific image sizes. Depending on the specified pipeline will change its expected and output dimensions
+    '''
+    print(f"Rearranging {gif}")
     im = np.asarray(Image.open(gif))
 
     if pipeline == "xpcd":
@@ -107,45 +159,64 @@ def rearrange_image(gif, output_dir, pipeline):
     return
 
 def process_imgs(dir, pipeline):
-    process = subprocess.Popen(['ls', dir], stdout=subprocess.PIPE, universal_newlines=True)
-    process.wait()
-    with process.stdout:
-        for file in process.stdout:
-            file = file[:-1] # strip \n character
-            if file.endswith('.gif'):
-                filename = os.path.join(dir, file)
-                rearrange_image(filename, dir, pipeline)
+    '''
+    finds all .gif files in the directory and calls rearrange_image on them
+    '''
+    try:
+        print(f"Processing images in {dir}")
+        process = subprocess.Popen(['ls', dir], stdout=subprocess.PIPE, universal_newlines=True)
+        process.wait()
+        with process.stdout:
+            for file in process.stdout:
+                file = file[:-1] # strip \n character
+                if file.endswith('.gif'):
+                    filename = os.path.join(dir, file)
+                    rearrange_image(filename, dir, pipeline)
+    except Exception as e:
+        print("Error processing images:")
+        print(e)
 
 def upload_imgs(dir, bucket):
-    process = subprocess.Popen(['ls', dir], stdout=subprocess.PIPE, universal_newlines=True)
-    process.wait()
-    with process.stdout:
-        for file in process.stdout:
-            file = file[:-1] # strip \n character
-            if file.endswith('.png'):
-                cmd = ["s3cmd", "put", file, bucket]
-                process2 = subprocess.check_call(cmd, cwd=f"./{dir}", stdout=subprocess.DEVNULL)
+    '''
+    calls s3cmd to upload all .png images in `dir` to `bucket`
+    '''
+    try:
+        print(f"Uploading images to S3")
+        process = subprocess.Popen(['ls', dir], stdout=subprocess.PIPE, universal_newlines=True)
+        process.wait()
+        with process.stdout:
+            for file in process.stdout:
+                file = file[:-1] # strip \n character
+                if file.endswith('.png'):
+                    cmd = ["s3cmd", "put", file, bucket]
+                    subprocess.check_call(cmd, cwd=f"./{dir}", stdout=subprocess.DEVNULL)
+    except Exception as e:
+        print("Error uploading images:")
+        print(e)
 
 def main():
     args = get_args()
     subjects = get_subjects(args)
     num_subjects = len(subjects)
-    bucket = "s3://" + args.destination
     pipeline = args.pipeline
 
     for index, subject in enumerate(subjects):
         if index + 1 < args.start:
             continue
+        if args.stop:
+            if index + 1 > args.stop:
+                break
         sessions = get_sessions(subject, args)
         for session in sessions:
             dir = subject + '-' + session
             print(f"Processing {subject}/{session}")
             download_imgs(subject, session, dir, args)
             process_imgs(dir, pipeline)
-            upload_imgs(dir, bucket)
-            cleanup(dir)
+            upload_imgs(dir, args.destination)
+            if not args.no_cleanup:
+                cleanup(dir)
 
-        print(f"completed {index + 1} of {num_subjects}")
+        print(f"completed subject {index + 1} of {num_subjects}")
 
 
 
