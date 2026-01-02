5fbbdba480c4da80e8c84a9573292a12eeb550c1
Joan Mendoza <joanmemo@gmail.com>
2026-01-02 10:57:17 -0600

    Updates the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
     <b-container class="about">
       <h1> Origin of Swipes For Science </h1>
         <p>
-          This application is built on the foundation of SwipesForScience, which is an open source project that provides a template for citizen science games. This tool makes it easy to create and customize the "game" to any dataset. Visit the <a href="https://swipesforscience.org/" target="_blank">SwipesForScience website</a> to learn more. 
+          This application is built on the foundation of SwipesForScience, which is an open source project that provides a template for citizen science games. This tool makes it easy to create and customize the "game" to any dataset. Visit the <a href="https://swipesforscience.org/" target="_blank">SwipesForScience website</a> to learn more.
         </p>
       <h1> Who are we? </h1>
         <p>
@@ -19,10 +19,10 @@
         </p>
       <h1> How does it work? </h1>
         <p>
-          BrainSwipes brings Tinder's swipe paradigm to quality control. For each image, you will "swipe" right to fail an image or "swipe" left to pass an image. You can either use the buttons on the image or your arrow keys to rate an image. To get started, you'll need to complete the required tutorials first. Our goal for each dataset is to have every image swiped on at least 10 times in order to reach a consenus on the quality of the image. This means you will not be the one and only rater on an image, so try not to worry too hard about whether or not you are rating it "correctly". As long as you are following the guidelines in the tutorials, you are helping! 
+          BrainSwipes brings Tinder's swipe paradigm to quality control. For each image, you will "swipe" right to fail an image or "swipe" left to pass an image. You can either use the buttons on the image or your arrow keys to rate an image. To get started, you'll need to complete the required tutorials first. Our goal for each dataset is to have every image swiped on at least 10 times in order to reach a consenus on the quality of the image. This means you will not be the one and only rater on an image, so try not to worry too hard about whether or not you are rating it "correctly". As long as you are following the guidelines in the tutorials, you are helping!
         </p>
         <p>
-          Once a dataset is QC'd, the experts will look at the ratings for each slice of each subject to determine if the subject should pass or fail as a whole. On a single image, we consider a score of above 0.7 a definite pass and a score of 0.3 a definite fail. Anything in between is questionable. Each subject has multiple images within the dataset which shows different views and slices of the same data. Since we are only looking at a single slice at a time, it can be hard to determine if the entire brain has issues or if this is just a weird transition slice that makes the data look worse than it is. All of the data will be considered when determining the quality of a subject. For example, there are generally 8 different anatomical images per subjects. If 1 image fails, while the rest of the images pass, then the subject as a whole would be considered a pass.  
+          Once a dataset is QC'd, the experts will look at the ratings for each slice of each subject to determine if the subject should pass or fail as a whole. On a single image, we consider a score of above 0.7 a definite pass and a score of 0.3 a definite fail. Anything in between is questionable. Each subject has multiple images within the dataset which shows different views and slices of the same data. Since we are only looking at a single slice at a time, it can be hard to determine if the entire brain has issues or if this is just a weird transition slice that makes the data look worse than it is. All of the data will be considered when determining the quality of a subject. For example, there are generally 8 different anatomical images per subjects. If 1 image fails, while the rest of the images pass, then the subject as a whole would be considered a pass.
         </p>
       <h1> Accessing Closed Studies </h1>
         <p>
@@ -83,8 +83,12 @@ export default {
     },
   },
   computed: {
+    /**
+     * Skip TEST and LearnExamples Studies
+     */
     filteredDatasets() {
-      return Object.keys(this.config.studies).filter(study => study !== 'TEST');
+      return Object.keys(this.config.studies)
+        .filter(study => !['TEST', 'LearnExamples'].includes(study));
     },
   },
 };
